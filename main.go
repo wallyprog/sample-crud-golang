1d104bcd98b27b27b705031815d19d875f2f7e21
wallyprog <wallysonprogrammer@gmail.com>
2021-05-26 10:15:32 -0300

    feat : add hello world

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(rw, "Hello World")
+	})
+
+	http.ListenAndServe(":8000", nil) // DefaultServerMux
+
+}
